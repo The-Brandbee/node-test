3a6b533a010d09381a0d3211c3881aab1dfd680f
The-Brandbee <digital@thebrandbee.com>
2023-12-24 16:57:00 +0530

    new

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,7 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
-import Script from "next/script";
+import GoogleAnalytics from "@bradgarropy/next-google-analytics";
+
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
@@ -11,27 +12,8 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <head>
-        <Script strategy="afterInteractive">
-          {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':` +
-            `new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],` +
-            `j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=` +
-            `'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);` +
-            `})(window,document,'script','dataLayer','GTM-PVLGJK24');`}
-        </Script>
-      </head>
-      <body className={inter.className}>
-        <noscript>
-          <iframe
-            src="https://www.googletagmanager.com/ns.html?id=GTM-PVLGJK24"
-            height="0"
-            width="0"
-            style="display:none;visibility:hidden"
-          ></iframe>
-        </noscript>
-
-        {children}
-      </body>
+      <body className={inter.className}>{children}</body>
+      <GoogleAnalytics measurementId="GTM-PVLGJK24" />
     </html>
   );
 }
