6e61e1883645cec0e7a486e8713a9a36852296bd
The-Brandbee <digital@thebrandbee.com>
2024-02-12 12:04:32 +0530

    new new

--- src/app/component/Post.js
+++ src/app/component/Post.js
@@ -11,40 +11,44 @@ const Posts = ({ posts, loading }) => {
 
   return (
     <>
-      {posts.map((post) => (
-        <div key={post.id}>
-          <div className="presscoverage-main-frame-middle">
-            <div className="presscoverage-main-frame-middle-left">
-              <div className="presscoverage-main-frame-middle-left-heading">
-                <h3 className="none">Category</h3>
-                <h2>{post.heading}</h2>
+      {posts
+        .map((post) => (
+          <div key={post.id}>
+            <div className="presscoverage-main-frame-middle">
+              <div className="presscoverage-main-frame-middle-left">
+                <div className="presscoverage-main-frame-middle-left-heading">
+                  <h3 className="none">Category</h3>
+                  <h2>{post.heading}</h2>
+                </div>
+                <div className="presscoverage-main-frame-middle-left-heading-middle">
+                  <h5>
+                    <Image src={Cal} alt="" /> {post.date}
+                  </h5>
+                  <p>{post.description} ...</p>
+                </div>
+                <div className="track-your-college-left-link">
+                  <a
+                    className="footer-top-map-img-button-link"
+                    href={`/blog-detail?blogid=${post._id}`}
+                  >
+                    {" "}
+                    Read more{" "}
+                    <span>
+                      <Image src={ArrowOne} className="normal" alt="" />
+                      <Image src={Arrowtwo} className="normal-ho" alt="" />
+                    </span>
+                  </a>
+                </div>
               </div>
-              <div className="presscoverage-main-frame-middle-left-heading-middle">
-                <h5>
-                  <Image src={Cal} alt="" /> {post.date}
-                </h5>
-                <p>{post.description} ...</p>
+              <div className="presscoverage-main-frame-middle-right">
+                <img
+                  src={"https://cms.csplighthouse.com/" + post.detailimage}
+                />
               </div>
-              <div className="track-your-college-left-link">
-                <a
-                  className="footer-top-map-img-button-link"
-                  href={`/blog-detail?blogid=${post._id}`}
-                >
-                  {" "}
-                  Read more{" "}
-                  <span>
-                    <Image src={ArrowOne} className="normal" alt="" />
-                    <Image src={Arrowtwo} className="normal-ho" alt="" />
-                  </span>
-                </a>
-              </div>
-            </div>
-            <div className="presscoverage-main-frame-middle-right">
-              <img src={"https://cms.csplighthouse.com/" + post.detailimage} />
             </div>
           </div>
-        </div>
-      ))}
+        ))
+        .reverse()}
     </>
   );
 };
