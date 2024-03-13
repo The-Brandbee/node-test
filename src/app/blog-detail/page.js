b11a55859e86c8fa927c69e458e1784e5c8350f6
The-Brandbee <digital@thebrandbee.com>
2024-03-13 15:35:34 +0530

    new

--- src/app/blog-detail/page.js
+++ src/app/blog-detail/page.js
@@ -7,14 +7,39 @@ export default function InnerBlog() {
     </>
   );
 }
-export const metadata = {
-  /*title: "",
-  description: "",*/
-  alternates: {
-    canonical: "https://csp.global/blog",
-  },
-  openGraph: {
-    images:
-      "https://cms.csplighthouse.com/public/uploads/blogs/blogs31a094f7cd7a260a28be8d150e4a99fc.png",
-  },
-};
+// export const metadata = {
+//   title: "Blog Detail",
+//   description: "",
+//   alternates: {
+//     canonical: "https://csp.global/blog",
+//   },
+//   openGraph: {
+//     images:
+//       "https://cms.csplighthouse.com/public/uploads/blogs/blogs31a094f7cd7a260a28be8d150e4a99fc.png",
+//   },
+// };
+export async function generateMetadata({ params, searchParams }) {
+  try {
+    const params = searchParams;
+    const blogId = params.blogid;
+
+    const blogResponse = await fetch(
+      "https://cms.csplighthouse.com/api/blogs/" + blogId
+    );
+
+    const blog = await blogResponse.json();
+
+    return {
+      title: blog.heading,
+      description: blog.description,
+      openGraph: {
+        images: blog.image,
+      },
+    };
+  } catch (error) {
+    console.log("Erorr", error);
+    return {
+      title: "CSP: Blog11",
+    };
+  }
+}
