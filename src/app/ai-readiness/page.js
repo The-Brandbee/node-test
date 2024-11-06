ce85a7a3681986323a3c0f048e84ffb02d977914
The-Brandbee <digital@thebrandbee.com>
2024-11-06 10:59:40 +0530

    new

---
+++ src/app/ai-readiness/page.js
@@ -0,0 +1,22 @@
+import SecurityMani from "../ai-readiness/ai-readiness.js";
+
+export const metadata = {
+  title: "Security Uplifts",
+  description:
+    "Explore CSP's cutting-edge cybersecurity solutions. Safeguard your business with advanced protection and proactive defence strategies.",
+  alternates: {
+    canonical: "https://csp.global/mdr",
+  },
+  openGraph: {
+    images:
+      "https://cms.csplighthouse.com/public/uploads/blogs/blogs31a094f7cd7a260a28be8d150e4a99fc.png",
+  },
+};
+
+export default function Security() {
+  return (
+    <>
+      <SecurityMani />
+    </>
+  );
+}
