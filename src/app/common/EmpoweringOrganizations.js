9aafdb8eeff59a0b2c4fb62cf845d8c59bed8e89
The-Brandbee <digital@thebrandbee.com>
2025-01-16 15:53:36 +0530

    new

--- src/app/common/EmpoweringOrganizations.js
+++ src/app/common/EmpoweringOrganizations.js
@@ -37,39 +37,36 @@ export default class TestimonialSlide extends Component {
           responsive={this.responsive}
         >
           <div className="Next-Gen-middle-frame">
-            <h3>Next-Gen Managed XDR</h3>
-            <p>
-              AI powered Technology. Human Guardians.
-              <br /> Microsoft Experts. Tailored for you!
+            <h3>RESOLUTION</h3>
+            <p>      
+            Our SLA's and subject matter expertise, arm your organization with the power to resolve threats and IT problems with unparalleled speed and accuracy. 
             </p>
             <div className="know-more">
               <Link href="/mdr">LEARN MORE</Link>
             </div>
           </div>
           <div className="Next-Gen-middle-frame">
-            <h3>Early Signs</h3>
+            <h3>THREAT INTEL</h3>
             <p>
-              We don't snooze so you don't lose. <br /> Early detection is
-              crucial.
+            As an ASD network partner and Microsoft Security Solutions Provider, we are primed with threat intelligence and security controls that will keep you safe around the clock. We present the 'what if' possibilities and show you how to tackle them.
             </p>
             <div className="know-more">
               <Link href="/about">LEARN MORE</Link>
             </div>
           </div>
           <div className="Next-Gen-middle-frame">
-            <h3>Beyond Technology</h3>
+            <h3>COST-EFFECTIVE</h3>
             <p>
-              Executive cyber reports that make sense. Analysts can verify risk
-              activity directly via Microsoft Teams with CSP "engage cards".
+            We bundle and tailor services which translates into savings without compromising on quality. We drive business outcomes through technology to ensure the greatest return on your investment.
             </p>
             <div className="know-more">
               <Link href="/about">LEARN MORE</Link>
             </div>
           </div>
           <div className="Next-Gen-middle-frame">
-            <h3>Think Globally Act Locally</h3>
+            <h3>STRATEGIC VISION</h3>
             <p>
-              Finger on the pulse. Threat intelligence <br /> designed for you!
+            Our vCIO and AI advisory services safeguard your future. We evaluate the integration of emerging technologies and prioritize the safety of your data and operations.
             </p>
             <div className="know-more">
               <Link href="/modern-soc">LEARN MORE</Link>
